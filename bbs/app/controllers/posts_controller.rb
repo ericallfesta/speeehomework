5efc423b7d8251217441ddb6f3f032d98bc657e7
ericfesta <eric@allfesta.com>
2015-08-05 20:28:54 +0900

    make gem test

--- bbs/app/controllers/posts_controller.rb
+++ bbs/app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order("created_at desc")
+    @posts = Post.order("created_at desc").page params[:page]
     @postslast = @posts.last(10)
 
   end
--- bbs/app/models/post.rb
+++ bbs/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+    paginates_per 10
     # 名前は必須入力
     validates :name ,  presence: true
     # コメントは必須入力
